46e717d3ae7a5ab58eae615a69868f420db7d5a9
AineasGamingYolo <aineas.vasiliou@gmail.com>
2020-12-22 20:57:47 +0200

    removed node_modules

--- pages/login.js
+++ pages/login.js
@@ -1,30 +1,13 @@
-import React from 'React'
 import * as Constants from '../constants'
 
 
 export default function Login() {
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    const user = {
-      email,
-      password
-    }
-
-    axios.post(Constants.LOGIN_API_URL, user)
-      .then(res => {
-        if (res.status === 200) {
-          console.log(res.data);
-          window.location = '/home';        
-        } else {
-          setErrMsg(res.data.error);
-        }
-      })
-      .catch(err => {
-        setErrMsg(err.response.data.error)
-        console.log(err.response.data.error);
-      });
-  }
+  
+  return (
+    <ul>
+      <h1>hi there login form is being worked in</h1>
+    </ul>
+  )
 
 }
